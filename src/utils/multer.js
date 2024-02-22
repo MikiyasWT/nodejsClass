860d7c678b6182904059a140d7fb00f1862f8dac
MikiyasWT <mikiywendu44@gmail.com>
2024-02-22 14:36:48 +0300

    image size limit and async stream created

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -5,7 +5,7 @@ const ApiError = require('./ApiError');
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    const filePath = `${__dirname}/../uploads`;
+    const filePath = `${__dirname}/../../uploads`;
     cb(null, filePath);
   },
   filename(req, file, cb) {
@@ -17,12 +17,17 @@ const storage = multer.diskStorage({
 module.exports = multer({
   storage,
   fileFilter(req, file, cb) {
+    const maxFileSize = 3 * 1024 * 1024;
     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
       return cb(
         new ApiError(httpStatus.BAD_REQUEST, 'Only images are allowed'),
         false,
       );
     }
-    cb(null, true);
+    if (file.size > maxFileSize) {
+      cb(new ApiError(httpStatus.BAD_REQUEST, "image can't excced 3MB"), false);
+    } else {
+      cb(null, true);
+    }
   },
 });
